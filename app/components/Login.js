a1cb6f05cbed903ed5158b218449f9ca1e3475bc
SunyoungKim508 <sunyoungkim508@gmail.com>
2016-01-26 11:46:33 -0800

    Message

--- app/components/Login.js
+++ app/components/Login.js
@@ -5,7 +5,8 @@ const Login = () => {
     <div className="row">
       <a href="/auth/twitter" style={{textDecoration: 'none', fontSize: 20, fontWeight: 300}}>
         <div className="alert alert-danger" style={{marginTop: 30}} role="alert">
-          This user account is private. Click this to login with Twitter
+          This user account is private. Click this to login with Twitter.
+          If you are logged in already, then you are not following this user, so you can't see the timeline.
         </div>
       </a>  
     </div>
--- public/bundle.js
+++ public/bundle.js
@@ -26388,7 +26388,7 @@
 	      _react2.default.createElement(
 	        "div",
 	        { className: "alert alert-danger", style: { marginTop: 30 }, role: "alert" },
-	        "This user account is private. Click this to login with Twitter"
+	        "This user account is private. Click this to login with Twitter. If you are logged in already, then you are not following this user, so you can't see the timeline."
 	      )
 	    )
 	  );
